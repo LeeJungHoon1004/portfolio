e306f5e0cfb6c1f4bb9f28ed9b32539bd4648993
Administrator <Administrator@406-09>
2017-08-10 10:53:10 +0900

    홈화면 & 회원가입(다이어로그) 업뎃1

--- src/Client/BasicShape.java
+++ src/Client/BasicShape.java
@@ -1,14 +1,20 @@
 package Client;
 
 import java.awt.BorderLayout;
+import java.awt.CardLayout;
 import java.awt.Color;
 import java.awt.Container;
-import java.awt.Dimension;
 import java.awt.Font;
-
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.net.Socket;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JPasswordField;
 import javax.swing.JScrollPane;
@@ -17,103 +23,194 @@ import javax.swing.UIManager;
 import javax.swing.UIManager.LookAndFeelInfo;
 import javax.swing.border.TitledBorder;
 
+
 public class BasicShape extends JFrame {
+	private Socket client;
+	private DataOutputStream dos;
+	private DataInputStream dis;
 
-   private Container cp = this.getContentPane();
-   private JLabel id1 = new JLabel("ID:");
-   private JTextField id2 = new JTextField();
-   private JLabel pw1 = new JLabel("PW:");
-   private JPasswordField pw2 = new JPasswordField();
-   private JButton membership = new JButton("회원가입");
-   private JButton in = new JButton("로그인");
+	// =======SOCKET========================
 
-   private JLabel title = new JLabel("title", JLabel.CENTER);
-   private Font font = new Font("바탕", Font.ITALIC, 30);
-   private JLabel loginshpe = new JLabel();
-   private JLabel category = new JLabel();
-   private JLabel picture = new JLabel();
-   private JLabel bmi = new JLabel();
-   private TitledBorder tborder;
+	private Container cp = this.getContentPane();
 
-   private JPanel pan = new JPanel();
-   private JScrollPane sc = new JScrollPane(pan);
-   
+	private JLabel lbID = new JLabel("아이디 :");
+	private JTextField inputID = new JTextField();
 
-   public void comp() {
-      setLayout(null);
-      
-      tborder = new TitledBorder("");
-      tborder.setTitlePosition(TitledBorder.ABOVE_TOP);// 지정한 위치에 타이틀을 나타내주는
-                                             // 보더
-      tborder.setTitleJustification(TitledBorder.CENTER);// 자리맞춤을 가운데로 지정
-      title.setBorder(tborder);
-      title.setFont(font);
-      loginshpe.setBorder(tborder);
-      category.setBorder(tborder);
-      picture.setBorder(tborder);
-      bmi.setBorder(tborder);
-      
-//      아직 추가 못한것들
-//      title.setBounds(370, 60, 1090, 160);
-//      loginshpe.setBounds(30, 120, 310, 280);
-//      category.setBounds(30, 420, 310, 520);
-//      picture.setBounds(370, 240, 1090, 360);
-//      bmi.setBounds(370, 640, 1090,2000);
-//      id1.setBounds(40, 160, 100, 60);
-//      id2.setBounds(150, 160, 170, 60);
-//      pw1.setBounds(40, 240, 100, 60);
-//      pw2.setBounds(150, 240, 170, 60);
-//      membership.setBounds(40, 310, 100, 60);
-//      in.setBounds(150, 310, 100, 60);
-      
-      
-      title.setPreferredSize(new Dimension(1090,160));
-      loginshpe.setPreferredSize(new Dimension(310,280));
-      category.setPreferredSize(new Dimension(310,520));
-      
-      
-      pan.add(title,BorderLayout.NORTH);
-      pan.add(loginshpe,BorderLayout.CENTER);
-      pan.add(category,BorderLayout.SOUTH);
-      
-//      아직 추가 못한것들      
-//      pan.add(picture);
-//      pan.add(bmi);
-//      pan.add(id1);
-//      pan.add(id2);
-//      pan.add(pw1);
-//      pan.add(pw2);
-//      pan.add(membership);
-//      pan.add(in);
-      
-      
-      this.sc.setBounds(0,0 ,1385, 865);
-      add(sc);
-   }
+	private JLabel lbPW = new JLabel("비밀번호 :");
+	private JPasswordField inputPW = new JPasswordField();
 
-   public BasicShape() {
-      setTitle("기본shape테스트");
-      setSize(1400, 900);
-      // setSize(1920,1080);
-      setLocationRelativeTo(null);
-      setDefaultCloseOperation(EXIT_ON_CLOSE);
-      cp.setBackground(Color.WHITE);
-      comp();
-      setVisible(true);
-   }
+	private JButton membership = new JButton("회원가입");
+	private JButton login = new JButton("로그인");
+	private JLabel title = new JLabel("title", JLabel.CENTER);
+	private Font font = new Font("바탕", Font.ITALIC, 30);
 
-   public static void main(String[] args) {
-      try {
-          for (LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
-              if ("Nimbus".equals(info.getName())) {
-                  UIManager.setLookAndFeel(info.getClassName());
-                  break;
-              }
-          }
-      } catch (Exception e) {
-          // If Nimbus is not available, you can set the GUI to another look and feel.
-      }
-      new BasicShape();
-   }// main end
+	private JButton goalBt = new JButton("나의 목표");
+	private JButton dailyBt = new JButton("하루 목표");
+	private JButton videoBt = new JButton("운동영상");
+	private JButton imgBoardBt = new JButton("사진게시판");
 
+	private JPanel category = new JPanel(new GridLayout(4, 1));
+	private JPanel picture = new JPanel();
+	private JPanel bmi = new JPanel();
+
+	private JPanel panbox1 = new JPanel(new GridLayout(1, 2));
+	private JPanel panbox2 = new JPanel(new GridLayout(1, 2));
+	private JPanel panbox3 = new JPanel(new GridLayout(1, 2));
+	private JPanel panbox = new JPanel(new GridLayout(3, 1));
+
+	private TitledBorder tborder;
+
+	private JPanel pan = new JPanel();
+	private JScrollPane sc = new JScrollPane(pan);
+	
+	private CardLayout card = new CardLayout();
+	private BasicShape self = this;
+
+	public void comp() {
+		setLayout(new BorderLayout());
+		// tborder = new TitledBorder("");
+		// tborder.setTitlePosition(TitledBorder.ABOVE_TOP);// 지정한 위치에 타이틀을
+		// 나타내주는 보더
+		// tborder.setTitleJustification(TitledBorder.CENTER);// 자리맞춤을 가운데로 지정
+		//
+		// title.setBorder(tborder);
+		// title.setFont(font);
+
+		category.add(goalBt);
+		category.add(dailyBt);
+		category.add(videoBt);
+		category.add(imgBoardBt);
+
+		panbox1.add(lbID);
+		panbox1.add(inputID);
+		panbox2.add(lbPW);
+		panbox2.add(inputPW);
+		panbox3.add(login);
+		panbox3.add(membership);
+		panbox.add(panbox1);
+		panbox.add(panbox2);
+		panbox.add(panbox3);
+
+		pan.add(panbox);
+		pan.add(category);
+
+		add(pan);
+		// //this.sc.setBounds(0, 0, 1185, 715);
+		// this.sc.setBounds(0, 0, 1185, 700);
+		//this.sc.setBounds(0, 0, 700, 500);
+		//add(sc, BorderLayout.CENTER);
+
+	}
+
+	public void changePanel() {
+		card.next(this.getContentPane());
+	}
+
+	public void eventInit() {
+		login.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				// 로그인
+				try {
+					client = new Socket("", 4000);
+					dos = new DataOutputStream(client.getOutputStream());
+					dis = new DataInputStream(client.getInputStream());
+					System.out.println("초기연결성공");
+				} catch (Exception e1) {
+					System.out.println("초기연결실패");
+				}
+
+				String userID = inputID.getText();
+				String userPW = inputPW.getText();
+
+				try {
+					dos.writeUTF("로그인");
+					dos.writeUTF(userID);
+					dos.writeUTF(userPW);
+					System.out.println("데이터보내기 성공");
+				} catch (Exception e1) {
+					System.out.println("데이터 보내기 실패");
+				}
+
+				try {
+					String result = dis.readUTF();
+
+					if (result.equals("로그인성공")) {
+						JOptionPane.showMessageDialog(null, "로그인 성공");
+						
+					} else if(result.equals("로그인실패")) {
+						JOptionPane.showMessageDialog(null, "로그인에 실패하였습니다.");
+					}
+					System.out.println("로그인 성공");
+				} catch (Exception e2) {
+					System.out.println("로그인 실패");
+				}
+			}
+		});
+
+		membership.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				// 회원가입창 띄우기
+				new SingUp(self).setVisible(true);
+			}
+		});
+
+		goalBt.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				// card.show(card,"goalPage");
+				// card.changePanel();
+				// 대충 이런식으로 쓴다.
+			}
+		});
+
+		dailyBt.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+
+			}
+		});
+
+		videoBt.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+
+			}
+		});
+
+		imgBoardBt.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+
+			}
+		});
+	}
+
+	public BasicShape() {
+		setTitle("기본shape테스트");
+		setSize(700, 500);
+		// setSize(1200, 750);
+		setLocationRelativeTo(null);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		cp.setBackground(Color.WHITE);
+		comp();
+		eventInit();
+		setVisible(true);
+	}
+
+	public static void main(String[] args) {
+		try {
+			for (LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
+				if ("Nimbus".equals(info.getName())) {
+					UIManager.setLookAndFeel(info.getClassName());
+					break;
+				}
+			}
+		} catch (Exception e) {
+			// If Nimbus is not available, you can set the GUI to another look
+			// and feel.
+		}
+		new BasicShape();
+	}// main end
 }
+
+// http://msource.tistory.com/5
+// 카드레이아웃 쉬운 설명
