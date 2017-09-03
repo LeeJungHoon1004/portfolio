d28f7fdc32acef276cc0b12fca72fb085e87e854
user10 <user10@user10-PC>
2017-09-03 13:59:52 +0900

    비디오 패널 고침

--- src/Client/VideoPan.java
+++ src/Client/VideoPan.java
@@ -29,7 +29,6 @@ import javax.swing.UIManager.LookAndFeelInfo;
 import javax.swing.border.TitledBorder;
 
 public class VideoPan extends JPanel {
-
 		
 	private TitledBorder yoga = new TitledBorder("");
 	private TitledBorder stretching = new TitledBorder("");
@@ -116,11 +115,13 @@ public class VideoPan extends JPanel {
 	
 	
 	private String[] urls = null;
+	private String[] splitN = null;
 	private String[] fileNames = null;
 	int[] fileSize = null;
 	byte[] filecontents = null;	
-	private String path = "C:/Users/Administrator/4weeksWorkout/";
+	private String path = "C:/Users/Administrator/4weeksWorkout";
 	private String[] imgpath;
+	private String[] urlButtons;
 	
 	
 	
@@ -184,45 +185,62 @@ public class VideoPan extends JPanel {
 	
 	
 	public void unmarsharlling() {
+		
 		try {
-			
-			dos.writeUTF("비디오패널 갱신");
+			 client = new Socket("127.0.0.1",40000);
+			 dis = new DataInputStream(client.getInputStream());
+			 dos = new DataOutputStream(client.getOutputStream());
+			}catch(Exception e1) {
+				System.out.println("초기연결 실패");
+			}
+		
+		
+		try {
+			dos.writeUTF("url데이터발신");
+			System.out.println(client.isClosed());
+			System.out.println(client.isConnected());
 			
 			ois = new ObjectInputStream(client.getInputStream());
-			
-			ArrayList < VideoFileList> vflList = new ArrayList < VideoFileList> ();
-			vflList =(ArrayList)ois.readObject();
-			
-			for(int i = 0 ; i< vflList.size() ; i++) {
-			
-			urls[i] = vflList.get(i).getUrl();
-			fileNames[i] = vflList.get(i).getFilename();
-			fileSize[i] = vflList.get(i).getFileSize();
-			filecontents =vflList.get(i).getFilecontents();
-			imgpath[i] = path+fileNames[i];
-			
-			System.out.println(urls[i]);
-			System.out.println(fileNames[i]);
-			System.out.println(fileSize[i]);
-			System.out.println(filecontents);
-			System.out.println(imgpath[i]);
-			
+
+			ArrayList<VideoFileList> vflList = new ArrayList<VideoFileList>();
+			vflList = (ArrayList<VideoFileList>) ois.readObject();
+			System.out.println("ois로 리스트 전달받기 성공");
+			System.out.println(vflList.size());
+			for (int i = 0; i < vflList.size(); i++) {
+				//names[i] = 
+				//fnames[i] = names[i].split("_");
+				urls[i] = vflList.get(i).getUrlPath();
+				splitN[i] = vflList.get(i).getUrlFileName();
+				fileNames[i] = splitN[1];
+				urlButtons [i] = vflList.get(i).getUrlButtonName();
+				fileSize[i] = vflList.get(i).getUrlFileSize();
+				filecontents = vflList.get(i).getFileContents();
+
+				imgpath[i] = path +"/"+ fileNames[i];
+
+				System.out.println("url 데이터 : "+urls);
+				System.out.println("fileNames 데이터 : "+fileNames);
+				System.out.println("urlButtons 데이터 : "+urlButtons);
+				System.out.println("fileSize 데이터 : "+fileSize);
+				System.out.println("filecontents 데이터 : "+filecontents);
+				//System.out.println(imgpath[i]);
+				System.out.println("====================");
 			}
-			//배열에 데이터 넣기완료
+			// 배열에 데이터 넣기완료
 			System.out.println("배열에 데이터 넣기 완료");
-			
-			File f = new File("");
+
+			File f = new File(path);
 			fos = new FileOutputStream(f);
 			bos = new BufferedOutputStream(fos);
 			dos = new DataOutputStream(bos);
 			dos.write(filecontents);
 			dos.flush();
-			
+
 			System.out.println("비디오패널 언마셜링 성공");
-			
+
 			dos.close();
-		}catch(Exception e) {
-			System.out.println("비디오패널 언마셜링 실패");
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
 	}
 	
